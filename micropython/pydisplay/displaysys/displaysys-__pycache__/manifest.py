4f593b34507f2c45b3ff9b4203862d08c813fa76
Brad Barnett <bbarnett@gmail.com>
2026-06-18 14:31:14 -0500

    pydisplay: testing.
    Signed-off-by: Brad Barnett <bbarnett@gmail.com>

--- micropython/pydisplay/displaysys/displaysys-__pycache__/manifest.py
+++
@@ -1,9 +0,0 @@
-metadata(
-    description="PyDisplay displaysys-__pycache__",
-    version="0.0.1",
-    author="Brad Barnett <contact@pydevices.com>",
-    license="MIT",
-    pypi_publish="displaysys-__pycache__",
-)
-require("displaysys")
-package("displaysys")
